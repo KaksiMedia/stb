602470854096057c9690ce455dff4dfd3db19df1
josefreitas2 <josefreitas2@gmail.com>
2015-11-24 17:31:22 +0000

    Create header.php
    header.php antes doubleclick

---
+++ header.php
@@ -0,0 +1,61 @@
+<!doctype html >
+<!--[if IE 8]>    <html class="ie8" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
+<head>
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <meta charset="<?php bloginfo( 'charset' );?>" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php
+    wp_head(); /** we hook up in wp_booster @see td_wp_booster_functions::hook_wp_head */
+    ?>
+</head>
+
+<script>
+/**
+* Função que controla um clique num link de saída no Google Analytics.
+* Esta função seleciona uma cadeia de URL válida como um argumento e utiliza essa cadeia de URL
+* como a etiqueta do evento.
+*/
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+     function () {
+     document.location = url;
+     }
+   });
+}
+</script>
+
+<body <?php body_class() ?> itemscope="itemscope" itemtype="<?php echo td_global::$http_or_https?>://schema.org/WebPage">
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/pt_PT/sdk.js#xfbml=1&version=v2.3";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<?php //this is closing in the footer.php file ?>
+
+<?php /* scroll to top */?>
+<div class="td-scroll-up"><i class="td-icon-menu-up"></i></div>
+
+<div id="td-outer-wrap">
+
+    <div class="td-transition-content-and-menu td-mobile-nav-wrap">
+        <?php locate_template('parts/menu-mobile.php', true);?>
+    </div>
+
+    <?php //this is closing in the footer.php file ?>
+    <div class="td-transition-content-and-menu td-content-wrap">
+
+
+
+<?php
+td_api_header_style::_helper_show_header();
+
+do_action('td_wp_booster_after_header'); //used by unique articles
